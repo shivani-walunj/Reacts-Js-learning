ee12e61c16d4656f8432df491b0794e5b6bd5bd2
shivani-walunj <shivaniiwalunj@gmail.com>
2024-04-22 10:59:04 +0530

    Add Initial project files

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//function Greet(){
+//   return <h1> Hello Shivani </h1>
+// }
+const Greet = () => {
+  return <h1> Functional Component </h1>;
+};
+
+export default Greet;
